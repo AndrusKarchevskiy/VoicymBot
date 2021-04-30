2e4b2a375c33e2ebcdbfb8696a1d7f7e33948ef8
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-04-30 18:27:26 +0300

    Updated gitignore, requirements. Improved some messages

--- recognition_audio/text_generator.py
+++ recognition_audio/text_generator.py
@@ -7,8 +7,8 @@ def get_text_from_voice(user_id, quantity_sent_audio_files):
     user_voice = speech_rec.AudioFile(f'data/voices/users_voices/{user_id}_{quantity_sent_audio_files}.wav')
     with user_voice as audio_file:
         audio_content = recog.record(audio_file)
-        ready_text = '✔Вот, что мне удалось распознать:\n' \
-                     '👉'
+        ready_text = '✔ Вот, что мне удалось распознать:\n\n' \
+                     '👉 '
         try:
             ready_text += f'{recog.recognize_google(audio_content, language="ru-RU")}'
         except speech_rec.UnknownValueError:
